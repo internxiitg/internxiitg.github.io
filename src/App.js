3c815d73d22bd99978048263bb4aa16ac1fc8dae
aryan-verma <aryanv2001@gmail.com>
2020-07-30 18:44:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='navclass container-fluid'>
+          <div className='row'>
+            <div className='col-2'>
+            </div>
+            <div className='col-6'>
+
+            </div>
+            <div className='col-2'>
+            <Link to='/' className='navlinks unselectable mr-4'>Home</Link>
+            <Link to='/' className='navlinks unselectable'>About</Link>
+            </div>
+            <div className='col'>
+
+            </div>
+          </div>
+        </div>
+        <div className="header-box container-fluid">
+        <div className="header-container">
+        <div className="header-title-intro">
+            <div className="title-header">The Internship Experience</div>
+            <div className="intro-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+          </div>
+          <div className="empty-container"></div>
+          </div>
+          <div className='header-buttons container'>
+          <button className='button2019'>
+            2019
+          </button>
+          <button className='button2020'>
+            2020
+          </button>
+          </div>
+        </div>
+    </Router>
   );
 }
 
