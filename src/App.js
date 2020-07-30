b0ee7210955e2b19c6719a31d324362f4037e2ed
aryan-verma <aryanv2001@gmail.com>
2020-07-30 20:47:37 +0530

    upper feed commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import UpperFeed from './components/upperfeed.component';
+import Feed from './components/feed.component';
 
-function App() {
-  return (
-    <Router>
-      <div className='navclass container-fluid'>
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className='navclass container-fluid'>
           <div className='row'>
             <div className='col-2'>
             </div>
@@ -14,33 +17,41 @@ function App() {
 
             </div>
             <div className='col-2'>
-            <Link to='/' className='navlinks unselectable mr-4'>Home</Link>
-            <Link to='/' className='navlinks unselectable'>About</Link>
+              <Link to='/' className='navlinks unselectable mr-4'>Home</Link>
+              <Link to='/' className='navlinks unselectable'>About</Link>
             </div>
             <div className='col'>
 
             </div>
           </div>
         </div>
-        <div className="header-box container-fluid">
-        <div className="header-container">
-        <div className="header-title-intro">
-            <div className="title-header">The Internship Experience</div>
-            <div className="intro-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
-          </div>
-          <div className="empty-container"></div>
+        <div className="header-box container-fluid unselectable">
+          <div className="header-container">
+            <div className="header-title-intro">
+              <div className="title-header">The Internship Experience</div>
+              <div className="intro-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+            </div>
+            <div className="empty-container"></div>
           </div>
           <div className='header-buttons container'>
-          <button className='button1 button2019'>
-            2019
+            <button className='button1 button2019'>
+              2019
           </button>
-          <button className='button1 button2020'>
-            2020
+            <button className='button1 button2020'>
+              2020
           </button>
           </div>
         </div>
-    </Router>
-  );
+
+        <UpperFeed />
+
+        <div className='footer-box unselectable'>
+          The Internship Experience ©
+      </div>
+      <Feed />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/upperfeed.component.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class UpperFeed extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (<div className="container">
+
+        <div className="upperfeed-box unselectable">
+
+          <div className="up-1">
+            <div className="up1-image" style={{backgroundImage: 'url('+'https://studyabroad.shiksha.com/mediadata/images/articles/university-of-pennsylvania-picture-id594949892.jpg' + ')'}}></div>
+            <div className="up1-content">
+              <div></div>
+              <div className="up1-title">An Adventure Abroad - University of Pennsylvania</div>
+              <div className="up1-desc">Are you interested in a research-related career, a PhD, or studying abroad? Are you unsure of your interests or career direction? My experiences preceding and including my internship at the University of Pennsylvania, in Philadelphia, PA, USA may shed light and help guide you in making your decisions.</div>
+            </div>
+          </div>
+          <div className="up-2">
+          <div className="up2-image" style={{backgroundImage: 'url('+'https://www.teektalks.com/wp-content/uploads/2018/03/Microsoft.jpg' + ')'}}></div>
+          <div className="up2-content"><div className="up2-title">A typical day in the life of a Microsoft Intern</div></div>
+          </div>
+          <div className="up-2">
+          <div className="up2-image" style={{backgroundImage: 'url('+'https://www.impossible.sg/wp-content/uploads/2018/05/Google-Digital-Marketing.jpg' + ')'}}></div>
+            <div className="up2-content"><div className="up2-title">How I made it to google, twice!</div></div>
+          </div>
+
+        </div>
+
+      </div>);
+    }
+}
+
+export default UpperFeed;
