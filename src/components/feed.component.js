44dbc7e956d3800e50b2d05d2bf413b235c06eed
aryan-verma <aryanv2001@gmail.com>
2020-08-01 18:14:46 +0530

    medium embedded

--- src/components/feed.component.js
+++ src/components/feed.component.js
@@ -1,68 +1,58 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+
+function removeTags(str) {
+    if ((str === null) || (str === ''))
+        return 'empty';
+    else
+        str = str.toString();
+    return str.replace(/(<([^>]+)>)/ig, '');
+}
+
+const FeedCard = props => (
+    <div className="feed-box">
+        <div className="feedbox-content">
+            <div className="feedbox-title">{props.post.title}</div>
+            <div className="feedbox-desc">{removeTags(props.post.content).substring(0, 400) + "...."}</div>
+            <div className="feedbox-author-date">
+                <div className="feedbox-author">{props.post.author}</div>
+                <div className="feedbox-date">15 March 2020</div>
+            </div>
+        </div>
+        <div className="feedbox-image" style={{ backgroundImage: 'url(' + props.post.thumbnail + ')' }}></div>
+    </div>
+)
 
 class Feed extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            posts: []
+        }
     }
+
+    componentDidMount() {
+        axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fthe-internship-experience-iit-guwahati')
+            .then(response => {
+                this.setState({ posts: response.data.items });
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+    postCards() {
+        return this.state.posts.slice(3).map(function (currentPost, i) {
+            return <FeedCard post={currentPost} />;
+        })
+    }
+
     render() {
         return (<div className="container">
             <div className="feed-heading">LATEST</div>
             <div className="feed-container">
-
-
-            <div className="feed-box">
-                <div className="feedbox-content">
-                    <div className="feedbox-title">Interning at JP Morgan Chase — Mumbai</div>
-                    <div className="feedbox-desc">I consider myself lucky — extremely lucky, in fact, to have been able to intern at one of the most prestigious banking firms across the globe — J.P.Morgan. When I met the person who interviewed me in the office, I asked him what J.P.Morgan expects from job applicants. He simply replied: “You have been here for a month now, I think you can guess what kind of people we are looking for.” </div>
-                    <div className="feedbox-author-date">
-                    <div className="feedbox-author">By John Smith</div>
-                    <div className="feedbox-date">15 March 2020</div>
-                    </div>
-                </div>
-                <div className="feedbox-image" style={{backgroundImage: 'url('+'https://miro.medium.com/max/875/1*r2DU-NgT_5jUkyCfhJ7-Uw.jpeg' + ')'}}></div>
-            </div>
-
-            <div className="feed-box">
-                <div className="feedbox-content">
-                    <div className="feedbox-title">Interning at Microsoft — Swapnil Kothriwal</div>
-                    <div className="feedbox-desc">I secured an internship with Microsoft during on campus interviews. I would like to first share how I prepared for the interview.</div>
-                    <div className="feedbox-author-date">
-                    <div className="feedbox-author">By John Smith</div>
-                    <div className="feedbox-date">14 March 2020</div>
-                    </div>
-                </div>
-                <div className="feedbox-image" style={{backgroundImage: 'url('+'https://miro.medium.com/max/875/1*M-nNaQ6y_jaZmJNbAYs7PA.jpeg' + ')'}}></div>
-            </div>
-
-            <div className="feed-box">
-                <div className="feedbox-content">
-                    <div className="feedbox-title">McGill University — Shwetank Panwar</div>
-                    <div className="feedbox-desc">Ever wondered about how the things you keep doing at a campus help you or don’t help you in the real world? Is there a more fun way to learn about the area you are interested in? Can you work on the bleeding edge of the field & unexplored questions while being an undergraduate? More importantly, how is living and working in a foreign land feels like?</div>
-                    <div className="feedbox-author-date">
-                    <div className="feedbox-author">By John Smith</div>
-                    <div className="feedbox-date">12 March 2020</div>
-                    </div>
-                </div>
-                <div className="feedbox-image" style={{backgroundImage: 'url('+'https://miro.medium.com/max/875/1*TL7cSs2_vaL3wOlUQULR7A.jpeg' + ')'}}></div>
-            </div>
-
-            <div className="feed-box">
-                <div className="feedbox-content">
-                    <div className="feedbox-title">Internship Experience: NVidia</div>
-                    <div className="feedbox-desc">Nvidia is based out of Santa Clara and comes to IIT Guwahati to offer internships for the Bangalore office. Their selection process involves CV shortlisting followed by one technical and one HR interview. </div>
-                    <div className="feedbox-author-date">
-                    <div className="feedbox-author">By John Smith</div>
-                    <div className="feedbox-date">10 March 2020</div>
-                    </div>
-                </div>
-                <div className="feedbox-image" style={{backgroundImage: 'url('+'https://media.glassdoor.com/companyupdate/w600/7633/nvidia-companyupdate-1579655809069.jpg' + ')'}}></div>
-            </div>
-
-            
-
-
+                {this.postCards()}
             </div>
         </div>);
     }
--- src/components/upperfeed.component.js
+++ src/components/upperfeed.component.js
@@ -1,62 +1,112 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+
+
+function removeTags(str) {
+  if ((str===null) || (str===''))
+  return 'empty';
+  else
+  str = str.toString();
+  return str.replace( /(<([^>]+)>)/ig, '');
+}
 
 class UpperFeed extends React.Component {
 
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      post1: {
+        title: "",
+        author: "",
+        pubDate: "",
+        content: "",
+        link:"",
+        thumbnail:"",
+        
+      },
+      post2: {
+        title: "",
+        author: "",
+        pubDate: "",
+        content: "",
+        link:"",
+        thumbnail:"",
+      },
+      post3: {
+        title: "",
+        author: "",
+        pubDate: "",
+        content: "",
+        link:"",
+        thumbnail:"",
+      }
     }
-    render() {
-        return (<div className="container">
+  }
 
-        <div className="upperfeed-box unselectable">
+  
 
-          <div className="up-1">
-            <div className="up1-image" style={{backgroundImage: 'url('+'https://studyabroad.shiksha.com/mediadata/images/articles/university-of-pennsylvania-picture-id594949892.jpg' + ')'}}></div>
-            <div className="up1-content">
-              <div className="up1-title">An Adventure Abroad - University of Pennsylvania</div>
-              <div className="up1-author">By John Smith</div>
-              <div className="up1-desc">Are you interested in a research-related career, a PhD, or studying abroad? Are you unsure of your interests or career direction? My experiences preceding and including my internship at the University of Pennsylvania, in Philadelphia, PA, USA may shed light and help guide you in making your decisions.</div>
-              <div className="up1-author-date">
-              <div className="up1-author2">By John Smith</div>
+  componentDidMount() {
+    axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fthe-internship-experience-iit-guwahati')
+      .then(response => {
+        this.setState({ post1: response.data.items[0], post2:response.data.items[1], post3: response.data.items[2] });
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+
+    return (<div className="container">
+
+      <div className="upperfeed-box unselectable">
+
+        <div className="up-1">
+          <div className="up1-image" style={{ backgroundImage: 'url(' + this.state.post1.thumbnail + ')' }}></div>
+          <div className="up1-content">
+          <div className="up1-title">{this.state.post1.title}</div>
+            <div className="up1-author">{this.state.post1.author}</div>
+            <div className="up1-desc">{removeTags(this.state.post1.content).substring(0, 400) + "...."}</div>
+            <div className="up1-author-date">
+              <div className="up1-author2">{this.state.post1.author}</div>
               <div className="up1-date">31 March 2020</div>
-              </div>
             </div>
           </div>
-          <div className="up-2">
-          <div className="up2-image" style={{backgroundImage: 'url('+'https://www.teektalks.com/wp-content/uploads/2018/03/Microsoft.jpg' + ')'}}></div>
-          <div className="up2-content"><div className="up2-title">A typical day in the life of a Microsoft Intern</div>
-          <div className="up2-desc">Are you interested in a research-related career, a PhD, or studying abroad? Are you unsure of your interests or career direction? My experiences preceding and including my internship at the University of Pennsylvania, in Philadelphia, PA, USA may shed light and help guide you in making your decisions.</div>
+        </div>
+        <div className="up-2">
+          <div className="up2-image" style={{ backgroundImage: 'url(' + this.state.post2.thumbnail + ')' }}></div>
+          <div className="up2-content"><div className="up2-title">{this.state.post2.title}</div>
+            <div className="up2-desc">{removeTags(this.state.post2.content).substring(0, 300) + "...."}</div>
           </div>
           <div className="up2-author-date">
-              <div className="up2-author" >
-                By John Smith
+            <div className="up2-author" >
+            {this.state.post2.author}
               </div>
-              <div className="up2-date">
-                22 March 2020
+            <div className="up2-date">
+              22 March 2020
               </div>
-            </div>
           </div>
-
-          <div className="up-2">
-          <div className="up2-image" style={{backgroundImage: 'url('+'https://www.impossible.sg/wp-content/uploads/2018/05/Google-Digital-Marketing.jpg' + ')'}}></div>
-            <div className="up2-content"><div className="up2-title">How I made it to google, twice!</div>
-            <div className="up2-desc">Are you interested in a research-related career, a PhD, or studying abroad? Are you unsure of your interests or career direction? My experiences preceding and including my internship at the University of Pennsylvania, in Philadelphia, PA, USA may shed light and help guide you in making your decisions.</div>
-            </div>
-            <div className="up2-author-date">
-              <div className="up2-author" >
-                By John Smith
-              </div>
-              <div className="up2-date">
-                22 March 2020
-              </div>
-            </div>
-          </div>
-
         </div>
 
-      </div>);
-    }
+        <div className="up-2">
+          <div className="up2-image" style={{ backgroundImage: 'url(' + this.state.post3.thumbnail + ')' }}></div>
+          <div className="up2-content"><div className="up2-title">{this.state.post3.title}</div>
+            <div className="up2-desc">{removeTags(this.state.post3.content).substring(0, 300) + "...."}</div>
+          </div>
+          <div className="up2-author-date">
+            <div className="up2-author" >
+            {this.state.post3.author}
+              </div>
+            <div className="up2-date">
+              22 March 2020
+              </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>);
+  }
 }
 
 export default UpperFeed;
