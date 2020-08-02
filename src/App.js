1fa4ed485acc78ca25cf429672a608dd5fd87a4b
aryan-verma <aryanv2001@gmail.com>
2020-08-02 22:30:36 +0530

    search added, filter hide

--- src/App.js
+++ src/App.js
@@ -5,79 +5,129 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import UpperFeed from './components/upperfeed.component';
 import Feed from './components/feed.component';
+import SearchFeed from './components/searchfeed.component'
 
 class App extends React.Component {
 
-  constructor(props){
-      super(props);
-      this.state = {
-        year:9999
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: 9999,
+      SearchText: '',
+      searchactive: false
+    }
 
-      this.onClickButton2019 = this.onClickButton2019.bind(this);
-      this.onClickButton2020 = this.onClickButton2020.bind(this);
+    this.onClickButton2019 = this.onClickButton2019.bind(this);
+    this.onClickButton2020 = this.onClickButton2020.bind(this);
+    this.onChangeSearchText = this.onChangeSearchText.bind(this);
 
   }
 
-  onClickButton2019(e){
+  onClickButton2019(e) {
     console.log("button 2019 clicked")
-      this.setState({year:2019});
+    this.setState({ year: 2019 });
   }
 
-  onClickButton2020(e){
+  onClickButton2020(e) {
     console.log("button 2020 clicked")
-    this.setState({year:2020});
+    this.setState({ year: 2020 });
   }
+
+  onChangeSearchText(e){
+    if(e.target.value.trim() == ''){
+      this.setState({searchactive:false});
+    }
+    else{
+      this.setState({searchactive:true, SearchText:e.target.value.trim()});
+    }
+}
 
   render() {
-    console.log("year:" + this.state.year);
-
-    return (
-      <Router>
-
+    if(this.state.searchactive){
+      return(<Router>
+  
           <div className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-            <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-              <span className="navbar-toggler-icon"></span>
+            <button className="navbar-toggler ml-auto" data-toggle="collapse" data-target="#navbarMenu">
+              <i class="fas fa-bars"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarMenu">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-              <Link to='/' className='unselectable nav-link navlinks active'>Home</Link>
-              </li>
-              <li className="nav-item">
-              <Link to='/' className='unselectable nav-link navlinks active'>About</Link>
-              </li>
-            </ul>
-            </div> 
-
-          </div>
-        <div className="header-box container-fluid unselectable">
-          <div className="header-container">
-            <div className="header-title-intro">
-              <div className="title-header">The Internship Experience</div>
-              <div className="intro-header">The Internship Experience brings the truest stories from students interning in diverse streams, from Billion dollar MNCs to hottest startups, Foreign universities to Indian research.</div>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to='/' className='unselectable nav-link navlinks active'>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to='/' className='unselectable nav-link navlinks active'>About</Link>
+                </li>
+              </ul>
             </div>
-            <div className="empty-container"></div>
+  
           </div>
-          <div className='header-buttons container'>
-            <button onClick={this.onClickButton2019} className='button1 button2019'>
-              2019
-          </button>
-            <button onClick={this.onClickButton2020} className='button1 button2020'>
-              2020
-          </button>
+          <div className="header-box container-fluid unselectable">
+            <div className="header-container">
+              <div className="header-title-intro">
+                <div className="title-header">The Internship Experience</div>
+                <div className="intro-header">The Internship Experience brings the truest stories from students interning in diverse streams, from Billion dollar MNCs to hottest startups, Foreign universities to Indian research.</div>
+              </div>
+              <div className="empty-container"></div>
+            </div>
+            <form>
+              <input value={this.SearchText} onChange={this.onChangeSearchText} type='text' className='search-box' placeholder='Search By Category (Ex. AI, Finance)'></input>
+            </form>
           </div>
+          <div className="searchfeed-container">
+          <SearchFeed searchtext={this.state.SearchText} key={this.state.SearchText} />
+          </div>
+  
+          <div className='footer-box unselectable'>
+            The Internship Experience ©
         </div>
+  
+        </Router>)
+    }
+    else{
+      return (
+        <Router>
+  
+          <div className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+            <button className="navbar-toggler ml-auto" data-toggle="collapse" data-target="#navbarMenu">
+              <i class="fas fa-bars"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarMenu">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to='/' className='unselectable nav-link navlinks active'>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to='/' className='unselectable nav-link navlinks active'>About</Link>
+                </li>
+              </ul>
+            </div>
+  
+          </div>
+          <div className="header-box container-fluid unselectable">
+            <div className="header-container">
+              <div className="header-title-intro">
+                <div className="title-header">The Internship Experience</div>
+                <div className="intro-header">The Internship Experience brings the truest stories from students interning in diverse streams, from Billion dollar MNCs to hottest startups, Foreign universities to Indian research.</div>
+              </div>
+              <div className="empty-container"></div>
+            </div>
+            <form>
+              <input value={this.SearchText} onChange={this.onChangeSearchText} type='text' className='search-box' placeholder='Search By Category (Ex. AI, Finance)'></input>
+            </form>
+          </div>
+  
+          <UpperFeed year={this.state.year} key={this.state.year + 1} />
+          <Feed year={this.state.year} key={this.state.year + 2} />
+  
+          <div className='footer-box unselectable'>
+            The Internship Experience ©
+        </div>
+  
+        </Router>
+      );
 
-        <UpperFeed year={this.state.year} key={this.state.year + 1} />
-        <Feed year={this.state.year} key={this.state.year + 2} />
-
-        <div className='footer-box unselectable'>
-          The Internship Experience ©
-      </div>
-      
-      </Router>
-    );
+    }
   }
 }
 
---
+++ src/components/searchfeed.component.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+import ShortenText from '../utils/ShortenText';
+import ToText from '../utils/ToText';
+
+
+const FeedCard = props =>{ 
+    
+    var shortMonthName = new Intl.DateTimeFormat('en-US', {
+        month: 'short'
+      }).format;
+      let date = new Date(props.post.pubDate);
+      const publishDate = shortMonthName(date) + ' ' + date.getDate() + ',' + ' ' + date.getFullYear();
+    
+    return (
+    <a class='feedbox-links' href={props.post.link}><div className="feed-box">
+        <div className="feedbox-content">
+            <div className="feedbox-title">{ShortenText(props.post.title, 0, 50)}</div>
+            <div className="feedbox-desc">{ShortenText(ToText(props.post.content), 0, 320) + '...'}</div>
+            <div className="feedbox-author-date">
+                <div className="feedbox-author">By {props.post.author}</div>
+            <div className="feedbox-date">{publishDate}</div>
+            </div>
+        </div>
+        <div className="feedbox-image" style={{ backgroundImage: 'url(' + props.post.thumbnail + ')' }}></div>
+    </div>
+    </a>
+)}
+
+class SearchFeed extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        console.log("searchfeed got loaded");
+
+        axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fthe-internship-experience-iit-guwahati')
+        .then(response => {
+            let new_posts=[];
+            for(var p of response.data.items){
+              p.categories=p.categories.join(",").split("-").join(" ").split(",");
+              console.log(p.categories);
+              if(p.categories.includes(this.props.searchtext.toLowerCase().trim())){
+                new_posts.push(p);
+              }
+            }
+            console.log("----------");
+            console.log(response.data.items);
+            console.log(new_posts);
+            console.log("----------");
+
+            this.setState({ posts: new_posts });
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+    postCards() {
+        return this.state.posts.map(function (currentPost, i) {
+            return <FeedCard post={currentPost} />;
+        })
+    }
+
+    render() {
+        return (<div className="container">
+            <div className="searchfeed-heading">SEARCH RESULTS</div>
+            <div className="feed-container">
+                {this.postCards()}
+            </div>
+        </div>);
+    }
+}
+
+export default SearchFeed;
