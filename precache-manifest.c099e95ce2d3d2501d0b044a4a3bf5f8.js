9c151a9cd5810695406daa94e559c5276d131512
internship experience <internxiitg@gmail.com>
2020-08-04 17:43:39 +0530

    Updates

---
+++ precache-manifest.c099e95ce2d3d2501d0b044a4a3bf5f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff1b97ba236797e82f53f3cf16b5dbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "873581a0eaddb3afd8a6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aa563e5482f80ed17e58",
+    "url": "/static/css/main.2ebbacea.chunk.css"
+  },
+  {
+    "revision": "873581a0eaddb3afd8a6",
+    "url": "/static/js/2.0014abaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0014abaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa563e5482f80ed17e58",
+    "url": "/static/js/main.024813bb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "211e266cd708fe0846f087fbccd0ebb8",
+    "url": "/static/media/aniket_image.211e266c.jpeg"
+  },
+  {
+    "revision": "0113d199934b2ff439c944c54e534541",
+    "url": "/static/media/aryan_image.0113d199.jpg"
+  },
+  {
+    "revision": "2fdd824e7f860c3b4e15ae6705df8996",
+    "url": "/static/media/logo2.2fdd824e.jpg"
+  },
+  {
+    "revision": "6c63d14cfcb247c5b4bbf7269c7a2ed8",
+    "url": "/static/media/smita_image.6c63d14c.jpeg"
+  },
+  {
+    "revision": "9ff8cecdff024fbfea96e0cc701bdb21",
+    "url": "/static/media/utkarsh_image.9ff8cecd.jpeg"
+  }
+]);
